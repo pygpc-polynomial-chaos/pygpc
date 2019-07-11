9722f7995e835ba3a0cd248a7192d1efd6ea9e25
Lucas Possner <lpossner@cbs.mpg.de>
2019-07-11 14:22:41 +0200

    - [Update] version number

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ ext_modules = [
 
 
 setup(name='pygpc',
-      version='0.1.post9',
+      version='0.2.1',
       description='A sensitivity and uncertainty analysis toolbox for Python',
       author='Konstantin Weise',
       author_email='kweise@cbs.mpg.de',
