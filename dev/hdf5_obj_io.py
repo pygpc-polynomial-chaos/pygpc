b9e7be11391e78df6d25fc5fce1eb7b7a2eb0168
Lucas Possner <lpossner@cbs.mpg.de>
2020-01-31 18:22:58 +0100

    Deleted dev/ folder

--- dev/hdf5_obj_io.py
+++
@@ -1,8 +0,0 @@
-import pygpc
-
-gpc_pkl = pygpc.read_session_pkl("/home/kporzig/share/Dropbox/_share/gpc_hdf5/pygpc_test_4_MEStaticProjection_gpc.pkl")
-
-pygpc.write_session_hdf5(gpc_pkl, "/home/kporzig/share/Dropbox/_share/gpc_hdf5/pygpc_test_4_MEStaticProjection_gpc.hdf5")
-
-# gpc_hdf5 = pygpc.read_session_hdf5("/home/kporzig/share/Dropbox/_share/gpc_hdf5/pygpc_test_4_MEStaticProjection_gpc.hdf5")
-
