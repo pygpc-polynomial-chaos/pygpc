2838377e51b5b3573bd911d5d4677f47cc3bad17
konstantinweise <kporzig@gmail.com>
2021-11-24 14:35:38 +0100

    - push test

--- pyrates_build/net0/layer_1/op_2.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-from pyrates.backend.funcs import *
-def assign_add_2(I_exc,c_19,c_18,r_old_1,r_exc,I_exc_old_0,c_17):
-    I_exc[:] += np.multiply(c_19,np.subtract(np.add(np.multiply(c_18,r_old_1),r_exc),np.divide(I_exc_old_0,c_17)))
-    return I_exc
