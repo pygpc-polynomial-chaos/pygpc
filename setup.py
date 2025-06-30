dd5d306514e684d39bfd013ca55de820e9e4f545
lpossner <lpossner>
2025-06-30 10:01:46 +0200

    PyPI Version 0.4.2

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ extensions = [
 
 setup(
     name="pygpc",
-    version="0.4.1",
+    version="0.4.2",
     description="A sensitivity and uncertainty analysis toolbox for Python",
     author="Konstantin Weise",
     author_email="kweise@cbs.mpg.de",
